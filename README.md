aaf2bd51368e59e9bec6366107fe345183116b55
AriaNadzaty <aria.nadzaty@gmail.com>
2024-01-31 20:36:09 +0700

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# MY first git project 
+-ini adalah project git pertama
+-berisi code javascript
+
+```js
---
+++ index.js
@@ -0,0 +1,3 @@
+/* My First Git Project */
+console.log('hello world');
+console.log('welcome to my life');
